8851df378c55355e0190a5b3a12335936de7dfd4
Abskkk <iszhanghy@outlook.com>
2023-07-03 18:05:27 -0400

    增加自定义配置项识别

--- src/main/java/com/haoyu/wiki/controller/TestController.java
+++ src/main/java/com/haoyu/wiki/controller/TestController.java
@@ -1,14 +1,17 @@
 package com.haoyu.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+    @Value("${test.hello:TEST}")
+    private String testHello;
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World!";
+        return "Hello World!" + testHello;
     }
     @PostMapping("/hello/post")
     public String helloPost(String name) {
