f59da73181644c7323d9b8b310ce190cd087a951
Abskkk <iszhanghy@outlook.com>
2023-07-03 17:43:20 -0400

    增加hello接口测试test.http

--- src/main/java/com/haoyu/wiki/controller/TestController.java
+++ src/main/java/com/haoyu/wiki/controller/TestController.java
@@ -1,8 +1,7 @@
 package com.haoyu.wiki.controller;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,5 +9,9 @@ public class TestController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
+    }
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "Hello World! Post," + name;
     }
 }
