e567e982e5d6d3b0197c38fb5fcdcebbc423d42c
Abskkk <iszhanghy@outlook.com>
2023-07-07 01:25:40 -0400

    Make CopyUtil to encapsulate BeanUtils

--- src/main/java/com/haoyu/wiki/service/EbookService.java
+++ src/main/java/com/haoyu/wiki/service/EbookService.java
@@ -5,11 +5,10 @@ import com.haoyu.wiki.domain.EbookExample;
 import com.haoyu.wiki.mapper.EbookMapper;
 import com.haoyu.wiki.req.EbookReq;
 import com.haoyu.wiki.resp.EbookResp;
-import org.springframework.beans.BeanUtils;
+import com.haoyu.wiki.util.CopyUtil;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -23,13 +22,15 @@ public class EbookService {
         criteria.andNameLike("%" + req.getName() + "%");
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
         
-        List<EbookResp> respList = new ArrayList<>();
-        for (Ebook ebook : ebookList) {
-            EbookResp ebookResp = new EbookResp();
-            BeanUtils.copyProperties(ebook, ebookResp);
-            respList.add(ebookResp);
-        }
-
-        return respList;
+//        List<EbookResp> respList = new ArrayList<>();
+//        for (Ebook ebook : ebookList) {
+////            EbookResp ebookResp = new EbookResp();
+////            BeanUtils.copyProperties(ebook, ebookResp);
+//            //Object Copy
+//            EbookResp ebookResp = CopyUtil.copy(ebook, EbookResp.class);
+//            respList.add(ebookResp);
+//        }
+        List<EbookResp> list = CopyUtil.copyList(ebookList, EbookResp.class);
+        return list;
     }
 }
