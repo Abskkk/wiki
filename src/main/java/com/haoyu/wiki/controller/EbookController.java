25592c9e6c1d381f84e46fad3c810c4b5097b176
Abskkk <iszhanghy@outlook.com>
2023-07-07 00:08:44 -0400

    add general Class CommonResp

--- src/main/java/com/haoyu/wiki/controller/EbookController.java
+++ src/main/java/com/haoyu/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.haoyu.wiki.controller;
 
 import com.haoyu.wiki.domain.Ebook;
+import com.haoyu.wiki.resp.CommonResp;
 import com.haoyu.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +16,10 @@ public class EbookController {
     @Resource
     private EbookService ebookService;
     @GetMapping("/list")
-    public List<Ebook> list() {
-        return ebookService.list();
+    public CommonResp list() {
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list = ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 }
