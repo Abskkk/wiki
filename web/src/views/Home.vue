63b98e8cdbc6e8086adcac2adae7eb855b59de0e
Abskkk <iszhanghy@outlook.com>
2023-10-08 17:44:24 -0500

    Configuring the axios interceptor Printing request and return parameters

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -84,12 +84,10 @@ export default defineComponent({
     const ebooks1 = reactive({books: []});
 
     onMounted(() => {
-      console.log("onMounted");
       axios.get( "/ebook/list").then((response) => {
         const data = response.data;
         ebooks.value = data.content;
         ebooks1.books = data.content;
-        console.log(response);
       });
     });
 
