95d3ec4e6007fb142ea67ce1955cab21238f9e99
Abskkk <iszhanghy@outlook.com>
2023-10-08 22:40:31 -0500

    add AOP, delete filter and interceptor

--- src/main/java/com/haoyu/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/haoyu/wiki/config/SpringMvcConfig.java
@@ -1,21 +1,17 @@
-package com.haoyu.wiki.config;
-
-import com.haoyu.wiki.interceptor.LogInterceptor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-import javax.annotation.Resource;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Resource
-    LogInterceptor logInterceptor;
-
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/login");
-    }
-}
-
+//package com.haoyu.wiki.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Resource
+//    LogInterceptor logInterceptor;
+//
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**").excludePathPatterns("/login");
+//    }
+//}
+//
