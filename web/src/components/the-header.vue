744509fcf286ef19f35f491074a5e4b29de6ebae
Abskkk <iszhanghy@outlook.com>
2023-10-09 19:06:35 -0500

    Add admin page(page, router, menu)

--- web/src/components/the-header.vue
+++ web/src/components/the-header.vue
@@ -6,9 +6,15 @@
         mode="horizontal"
         :style="{ lineHeight: '64px' }"
     >
-      <a-menu-item key="1">nav 1</a-menu-item>
-      <a-menu-item key="2">nav 2</a-menu-item>
-      <a-menu-item key="3">nav 3</a-menu-item>
+      <a-menu-item key="/">
+        <router-link to="/">Home page</router-link>
+      </a-menu-item>
+      <a-menu-item key="/admin/ebook">
+        <router-link to="/admin/ebook">Admin</router-link>
+      </a-menu-item>
+      <a-menu-item key="/about">
+        <router-link to="/about">About us</router-link>
+      </a-menu-item>
     </a-menu>
   </a-layout-header>
 </template>
---
+++ web/src/views/admin/admin-ebook.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>Ebook Administration</h1>
+  </div>
+</template>
