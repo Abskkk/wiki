ea30d21484063e1b9a7936186fa25957f2052628
Abskkk <iszhanghy@outlook.com>
2023-11-05 18:08:37 -0600

    Integrate pageHelper

--- src/main/java/com/haoyu/wiki/service/EbookService.java
+++ src/main/java/com/haoyu/wiki/service/EbookService.java
@@ -1,12 +1,15 @@
 package com.haoyu.wiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.haoyu.wiki.domain.Ebook;
 import com.haoyu.wiki.domain.EbookExample;
 import com.haoyu.wiki.mapper.EbookMapper;
 import com.haoyu.wiki.req.EbookReq;
 import com.haoyu.wiki.resp.EbookResp;
 import com.haoyu.wiki.util.CopyUtil;
-import org.springframework.stereotype.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
@@ -14,6 +17,8 @@ import java.util.List;
 
 @Service
 public class EbookService {
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
+
     @Resource
     private EbookMapper ebookMapper;
 
@@ -23,8 +28,13 @@ public class EbookService {
         if (!ObjectUtils.isEmpty(req.getName())) {
             criteria.andNameLike("%" + req.getName() + "%");
         }
+        PageHelper.startPage(1, 3);
         List<Ebook> ebookList = ebookMapper.selectByExample(ebookExample);
-        
+
+        PageInfo<Ebook> pageInfo = new PageInfo<>();
+        LOG.info("总行数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
+
 //        List<EbookResp> respList = new ArrayList<>();
 //        for (Ebook ebook : ebookList) {
 ////            EbookResp ebookResp = new EbookResp();
